0bc35b41f676609191625ff53f937fd7817f7eeb
RIDDHI KAPOOR <100359128+RiddhiKapoor5501@users.noreply.github.com>
2024-07-14 16:16:09 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ProductPage from './Components/ProductPage';  // Updated path
+// import ProductPage from './Components/ProductPage1';  // Updated path
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <ProductPage />
+    </div>
+  );
+};
+
+export default App;
+
